ef626d90d337fa84742b7b215bedec9d30d13014
zyzCodes <diego.earanas1@gmail.com>
2022-08-01 16:07:02 -0600

    started db query set up 🚀

---
+++ environment.py
@@ -0,0 +1,37 @@
+import json
+
+class Environment:
+
+    def __init__(self, environment):
+        with open("secret.json", "r") as read_file:
+            credentials = json.load(read_file)
+            
+        self.environment = credentials['environment']
+        if environment == 'production':
+            self.server = credentials['server']
+            self.database = credentials['db']
+            self.username = credentials['username']
+            self.password = credentials['password']
+        elif environment == 'development':
+            self.server = credentials['server']
+            self.database = credentials['db']
+            self.username = credentials['username']
+            self.password = credentials['password']
+        else:
+            print('env does not exist')
+
+    def Environment(self):
+        return self.environment
+
+    def Server(self):
+        return self.server
+
+    def Database(self):
+        return self.database
+
+    def Username(self):
+        return self.username
+
+    def Password(self):
+        return self.password
+    
--- useful.py
+++ useful.py
@@ -1,13 +1,56 @@
 import json
+import datetime, time as utz
+from datetime import datetime as dt
 
 
-with open("data/filenames.json", "r") as read_file:
-    files = json.load(read_file)
 
+def getComponentType(changed_id):
+    with open("data/components.json", "r") as read_file:
+        components = json.load(read_file)
+    component_type=components[changed_id]
+    return component_type
 
 def getFile(changed_id):
+    with open("data/filenames.json", "r") as read_file:
+        files = json.load(read_file)  
     file=files[changed_id]
     return file
+
+def getProyectId(changed_id):
+    with open("data/proyectsid.json", "r") as read_file:
+        proyectsids = json.load(read_file)
+    project_id=proyectsids[changed_id]
+    return project_id
+    
+
+
+
+def days_between(d1, d2):
+    return abs((d2 - d1).days)
+
+def GetDatesToQuery(start_date, end_date, dropdown1, dropdown2, dropdown3, dropdown4):
+    if end_date is None:
+        now_mex_end = datetime.datetime.now()
+        now_mex_start = now_mex_end - datetime.timedelta(hours=8)
+        start_local_s = format(now_mex_start, '%Y-%m-%d %H:%M')
+        end_local_s = format(now_mex_end, '%Y-%m-%d %H:%M')
+        start = utz.local_datetime_to_utc(now_mex_start)
+        end = utz.local_datetime_to_utc(now_mex_end)
+        start = start.strftime('%Y-%m-%d %H:%M')
+        end = end.strftime('%Y-%m-%d %H:%M')
+    else:
+        start_local_s = str(start_date)[0:10]
+        start_local_s = start_local_s + ' ' + dropdown1 + ':' + dropdown2
+        end_local_s = end_date + ' ' + dropdown3 + ':' + dropdown4
+        start_local = dt.strptime(start_local_s, '%Y-%m-%d %H:%M')
+        end_local = dt.strptime(end_local_s, '%Y-%m-%d %H:%M')
+        if days_between(start_local, end_local) > 32:
+            return 'The date interval is it very large. Please insert a interval less that a month.'
+        start = utz.local_datetime_to_utc(start_local)
+        end = utz.local_datetime_to_utc(end_local)
+        start = start.strftime('%Y-%m-%d %H:%M')
+        end = end.strftime('%Y-%m-%d %H:%M')
+    return (start, end, start_local_s, end_local_s)
 
     
     
